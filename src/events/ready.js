a4899ab0975b3f193f6e42beab2eff8c0614980a
apherawashera <74432256+ApheraBy@users.noreply.github.com>
2023-05-04 00:24:12 +0300

    Delete src directory

--- src/events/ready.js
+++
@@ -1,8 +0,0 @@
-const registerCommands = require("../utils/registerCommands.js");
-
-module.exports = (client) => {
-
-    client.once("ready", async () => {       
-        registerCommands(client, "global")
-    })
-}
